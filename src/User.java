4dd92c28292b299320193881e738acfc6d4bfde4
Elizabeth Porter <ep22@students.uwf.edu>
2021-03-17 20:42:53 -0500

    cases

--- src/User.java
+++ src/User.java
@@ -30,4 +30,46 @@ public class User {
 			return true;
 		}
 	}
+
+	public void run() {
+		Scanner in = new Scanner(System.in);
+		String login;
+		String password;
+		while (true) {
+			System.out.println("1. Register an account.");
+			System.out.println("2. Login.");
+			System.out.println("0. Exit");
+			login = in.nextLine();
+			switch (login) {
+				case "1":
+					System.out.println("Enter login.");
+					login = in.nextLine();
+					System.out.println("Enter password");
+					password = in.nextLine();
+					if (registering(login, password)) {
+						System.out.println("You have successfully registered.");
+					} else {
+						System.out.println("The login is already in use by another user.");
+					}
+					break;
+				case "2":
+					System.out.println("Enter login.");
+					login = in.nextLine();
+					System.out.println("Enter password");
+					password = in.nextLine();
+					if (login(login, password)) {
+						System.out.println("You have successfully logged in.");
+					} else {
+						System.out.println("Invalid login and password combination.");
+					}
+					break;
+				case "0":
+					return;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new User();
+	}
 }
