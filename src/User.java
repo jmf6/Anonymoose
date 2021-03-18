0817acdd07b8c6c8b69ff31e4278e5108a58f44f
Elizabeth Porter <ep22@students.uwf.edu>
2021-03-17 20:07:10 -0500

    login

--- src/User.java
+++ src/User.java
@@ -8,4 +8,15 @@ public class User {
 		users = new HashMap<>();
 		run();
 	}
+
+	public boolean login(String login, String password) {
+		//check that there is such a login in the map
+		if (users.get(login) != null) {
+			//compare the password for this login with the password passed
+			//in the parameters and return the verification result
+			return users.get(login).equals(password);
+		} else {
+			return false;
+		}
+	}
 }
